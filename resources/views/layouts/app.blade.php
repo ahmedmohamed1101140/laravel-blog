66676ec0c6dc284b4f1375352a799a836d5cee70
ahmedmohamed110140 <ahmedmohamed1101140@outlook.com>
2020-11-30 21:29:53 +0200

    adjusting views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">\
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <title>MY Blog</title>
 </head>
-<body>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Home</a>
+                <a href="" class="p-3">Dashboard</a>
+                <a href="" class="p-3">Posts</a>
+            </li>
+        </ul>
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Ahmed Mabrouk</a>
+                <a href="" class="p-3">Login</a>
+                <a href="" class="p-3">Register</a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 </html>
